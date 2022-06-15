01dbf503fd87be427732c3882959a7df0cc4ff89
fgfo2018 <34053331+fgfo2018@users.noreply.github.com>
2022-06-15 13:40:37 +0800

    louis 0615_v4

--- Thermal_MO/layouts/default.vue
+++ Thermal_MO/layouts/default.vue
@@ -90,30 +90,30 @@ export default {
           // to: '5',
           to: 'PermissionPage',
         },
-        {
-          icon: 'mdi-account-group-outline',
-          title: '測試7:JQ日期時間Range選擇',
-          // to: '5',
-          to: 'ForTesting7',
-        },
-        {
-          icon: 'mdi-account-group-outline',
-          title: 'LouisAPI示範',
-          // to: '5',
-          to: 'ForTesting9',
-        },
-        {
-          icon: 'mdi-account-group-outline',
-          title: 'API動態測試(南部)',
-          // to: '5',
-          to: 'ForTesting11',
-        },
-        {
-          icon: 'mdi-account-group-outline',
-          title: 'API動態測試(後端)',
-          // to: '5',
-          to: 'ForTesting10',
-        },
+        // {
+        //   icon: 'mdi-account-group-outline',
+        //   title: '測試7:JQ日期時間Range選擇',
+        //   // to: '5',
+        //   to: 'ForTesting7',
+        // },
+        // {
+        //   icon: 'mdi-account-group-outline',
+        //   title: 'LouisAPI示範',
+        //   // to: '5',
+        //   to: 'ForTesting9',
+        // },
+        // {
+        //   icon: 'mdi-account-group-outline',
+        //   title: 'API動態測試(南部)',
+        //   // to: '5',
+        //   to: 'ForTesting11',
+        // },
+        // {
+        //   icon: 'mdi-account-group-outline',
+        //   title: 'API動態測試(後端)',
+        //   // to: '5',
+        //   to: 'ForTesting10',
+        // },
         // {
         //   icon: 'mdi-account-group-outline',
         //   title: '原API動態',
@@ -138,12 +138,12 @@ export default {
         //   // to: '5',
         //   to: 'ForTesting9',
         // },
-        {
-          icon: 'mdi-account-group-outline',
-          title: 'API測試(後端)',
-          // to: '5',
-          to: 'ForTesting13',
-        },
+        // {
+        //   icon: 'mdi-account-group-outline',
+        //   title: 'API測試(後端)',
+        //   // to: '5',
+        //   to: 'ForTesting13',
+        // },
         // {
         //   icon: 'mdi-account-group-outline',
         //   title: '測試9',
