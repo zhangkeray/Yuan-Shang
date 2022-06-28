1b8f45c7dd79c8ada1b37b569935ad458df4cb82
fgfo2018 <34053331+fgfo2018@users.noreply.github.com>
2022-06-28 10:20:49 +0800

    louis 0628_v1

--- Thermal_MO/pages/MultiScreen.vue
+++ Thermal_MO/pages/MultiScreen.vue
@@ -5,7 +5,20 @@
       <div>
         <v-row>
           <v-col cols="9" class="p-1">
-            <v-card class="camera-bg mt-3" style="height: 830px"> </v-card>
+            <v-card class="camera-bg mt-3" style="">
+              <div class="cam-grid">
+                <div v-for="(item01, index) in cam" :key="index">
+                  <img
+                    src="1656315342184.jpg"
+                    class="test-cramre"
+                    width="100%"
+                  />
+                </div>
+              </div>
+              <div class="cam-only">
+                <img src="1656315342184.jpg" class="test-cramre" width="100%" />
+              </div>
+            </v-card>
           </v-col>
           <v-col cols="3">
             <v-card class="camera-bg mt-3" style="height: 830px">
@@ -47,6 +60,9 @@
 <script>
 export default {
   name: 'MultiScreenPage',
+  data: () => ({
+    cam: ['1', '2', '3'],
+  }),
 }
 </script>
 
@@ -56,5 +72,17 @@ export default {
 }
 .camera-bg {
   background: #fff;
+  height: 830px;
+  padding: 16px;
+  align-items: center;
+  display: flex;
+}
+.cam-grid {
+  display: grid;
+  grid-template-columns: 338px;
+  margin-right: 7px;
+}
+.cam-only {
+  width: 100%;
 }
 </style>
